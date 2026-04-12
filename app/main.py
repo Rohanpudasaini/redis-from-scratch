8e6207d4ebe5126e989d56f6ded1c3ceabf1f56c
Rohan <pudasainirohan0@gmail.com>
2026-04-12 15:48:07 +0545

    feat: simple pong

--- app/main.py
+++ app/main.py
@@ -8,8 +8,8 @@ def main():
     # Uncomment the code below to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    simple_socket = server_socket.accept()  # wait for client
-    simple_socket[0].send(b"+PONG\r\n")
+    connection, _ = server_socket.accept()  # wait for client
+    connection.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
